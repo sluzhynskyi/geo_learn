f6846b233d97847d2924c8654b7e05f14bd47927
Danylo Sluzhynskyi <sluzhynskyi@gmail.com>
2021-06-26 01:49:50 +0300

    Added common Firebase auth functions

---
+++ lib/repositories/user_repository.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract class BaseAuth {
+  Future<User?> signIn(String email, String password);
+  Future<User?> signUp(String email, String password);
+  Future<void> signOut();
+  User? getCurrentUser();
+  bool isSignedIn();
+}
+
+class UserRepository implements BaseAuth {
+  final FirebaseAuth _firebaseAuth;
+
+  UserRepository() : _firebaseAuth = FirebaseAuth.instance;
+
+  Future<User?> signIn(String email, String password) async {
+    UserCredential result = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+
+    return result.user;
+  }
+
+  Future<User?> signUp(String email, String password) async {
+    UserCredential result = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+
+    return result.user;
+  }
+
+  Future<void> signOut() async {
+    return _firebaseAuth.signOut();
+  }
+
+  User? getCurrentUser() {
+    return _firebaseAuth.currentUser;
+  }
+
+  bool isSignedIn() {
+    final currentUser = _firebaseAuth.currentUser;
+
+    return currentUser != null;
+  }
+}

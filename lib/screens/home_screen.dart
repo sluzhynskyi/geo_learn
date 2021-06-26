948d07a4810bb6156a6599e50854032694dd2e41
Danylo Sluzhynskyi <sluzhynskyi@gmail.com>
2021-06-26 03:09:38 +0300

    Added initial HomeScreen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geo_learn/blocs/authentication_bloc/authentication_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  final User user;
+
+  const HomeScreen({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              BlocProvider.of<AuthenticationBloc>(context)
+                  .add(AuthenticationLoggedOut());
+            },
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Text("Hello, ${user.email}"),
+          ),
+        ],
+      ),
+    );
+  }
+}

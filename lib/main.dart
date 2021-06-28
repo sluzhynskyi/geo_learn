97744e42c3867ab00878b1088703824e739fd7e7
Danylo Sluzhynskyi <sluzhynskyi@gmail.com>
2021-06-28 23:18:04 +0300

    Added material creation function

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:geo_learn/screens/register/register_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:geo_learn/utils/material_color.dart';
 
 import 'package:provider/provider.dart';
 // import 'package:flutter_hooks/flutter_hooks.dart';
@@ -19,54 +19,43 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(
-    GeoLearn()
-  );
+  runApp(GeoLearn());
 }
 
-
-// final quizQuestionsProvider = FutureProvider.autoDispose<List<Question>>(
-//   (ref) => ref.watch(quizRepositoryProvider).getQuestions(
-//         numQuestions: 5,
-//         categoryId: 22,
-//         difficulty: Difficulty.any,
-//       ),
-// );
-
 class GeoLearn extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        // StreamProvider<Report>.value(stream: Global.reportRef.documentStream),
-        StreamProvider<User?>.value(value: FirebaseAuth.instance.authStateChanges(), initialData:null),
-        ],
-      child: AppRouter()
-      );
+    return MultiProvider(providers: [
+      // StreamProvider<Report>.value(stream: Global.reportRef.documentStream),
+      StreamProvider<User?>.value(
+          value: FirebaseAuth.instance.authStateChanges(), initialData: null),
+    ], child: AppRouter());
   }
 }
 
-class AppRouter extends StatelessWidget{
+class AppRouter extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     // User? user = Provider.of<User?>(context);
     // print(user);
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Flutter Demo",
-                // Named Routes
-        routes: {
-          '/': (context) => HomeScreen(),
-          '/login': (context) => LoginScreen(),
-          '/register': (context) => RegisterScreen(),
-          '/quiz': (context) => QuizScreen()
-          
-        }
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
+      // Named Routes
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegisterScreen(),
+        '/quiz': (context) => QuizScreen()
+      },
+      theme: ThemeData(
+        primarySwatch: createMaterialColor(Color(0xFFA5DC69)),
+        bottomSheetTheme:
+            const BottomSheetThemeData(backgroundColor: Colors.transparent),
+      ),
     );
   }
 }
-
 
 // class GeoLearn extends StatelessWidget {
 //   @override

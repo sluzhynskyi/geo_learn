dd3923e9d12664d04046a8fb1455afa2a6df9be9
Danylo Sluzhynskyi <sluzhynskyi@gmail.com>
2021-06-29 15:44:05 +0300

    fixed white block at the bottom

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:geo_learn/utils/validators.dart';
 import 'package:geo_learn/utils/auth.dart';
 import 'package:geo_learn/widgets/error.dart';
 
-
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -36,144 +35,157 @@ class LoginScreenState extends State<LoginScreen> {
     }
 
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: Column(
-        children: [
-          Container(
-           padding: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [Color(0xFF85B5E1), Color(0xFFE9F5E2)],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(30),
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [Color(0xFF85B5E1), Color(0xFFE9F5E2)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [ SizedBox(height: 20),
-              Image.asset('assets/images/planet-earth.png', height: 100, width: 100),
-              SizedBox(height: 20),  
-              Text(
-                  'GeoLearn',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  'Let’s learn  interesting facts\n about all Countries  in the World!',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 70),
-                TextFormField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.email),
-                    labelText: "Email",
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                    borderSide: BorderSide(color: Color.fromRGBO(0, 165, 220, 15), width: 2),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    borderSide: BorderSide(color: Color.fromRGBO(0, 165, 220, 15)),
-                  ),
-                ),
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  autocorrect: false,
-                  validator: (_) {
-                    return !Validators.isValidEmail(email)
-                        ? 'Invalid Email'
-                        : null;
-                  },
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.lock),
-                    labelText: "Password",
-                    
-                    enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                    borderSide: BorderSide(color: Color.fromRGBO(0, 165, 220, 15), width: 2),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    borderSide: BorderSide(color: Color.fromRGBO(0, 165, 220, 15)),
-                  ),
-                  ),
-                  obscureText: true,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  autocorrect: false,
-                  validator: (_) {
-                    return !Validators.isValidPassword(password)
-                        ? 'Invalid Password'
-                        : null;
-                  },
-                ),
-                SizedBox(height: 130),
-                Container(
-                  height: 50.0,
-                  width: 200.0,
-                  margin: EdgeInsets.all(10),
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Color.fromRGBO(0, 165, 220, 15))),
-                    padding: EdgeInsets.all(10.0),
-                    color: Color.fromRGBO(0, 165, 220, 1),
-                    textColor: Colors.white,
-                    child: Text('Log In', style: TextStyle(fontSize: 15)),
-                    onPressed: () async {
-                      try {
-                        var user = await Auth.signIn(email, password);
-                      } catch (error) {
-                        setState(() {
-                          loginError = "Incorrect password or e-mail";
-                        });
-                      }
-                      if (user != null) {
-                        Navigator.pushReplacementNamed(context, '/');
-                      }
-                    },
-                  ),
-                ),
-                Container(
-                  height: 50.0,
-                  width: 200.0,
-                    margin: EdgeInsets.only(bottom: 10),
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: BorderSide(color: Color.fromRGBO(0, 160, 227, 1))),
-                        padding: EdgeInsets.all(10.0),
-                        color: Colors.white,
-                        textColor: Color.fromRGBO(0, 160, 227, 1),
-                      child: Text('Sign Up', style: TextStyle(fontSize: 15)),
-                        onPressed: () async {
-                        Navigator.pushReplacementNamed(context, '/register');
+          ),
+          child: Column(
+            children: [
+              Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(height: 20),
+                    Image.asset('assets/images/planet-earth.png',
+                        height: 100, width: 100),
+                    SizedBox(height: 20),
+                    Text(
+                      'GeoLearn',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      'Let’s learn  interesting facts\n about all Countries  in the World!',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 70),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.email),
+                        labelText: "Email",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(0, 165, 220, 15), width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(0, 165, 220, 15)),
+                        ),
+                      ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      autocorrect: false,
+                      validator: (_) {
+                        return !Validators.isValidEmail(email)
+                            ? 'Invalid Email'
+                            : null;
                       },
-                    )
-                  ),
-                  ShowError(loginError)
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.lock),
+                        labelText: "Password",
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(0, 165, 220, 15), width: 2),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(0, 165, 220, 15)),
+                        ),
+                      ),
+                      obscureText: true,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      autocorrect: false,
+                      validator: (_) {
+                        return !Validators.isValidPassword(password)
+                            ? 'Invalid Password'
+                            : null;
+                      },
+                    ),
+                    SizedBox(height: 130),
+                    Container(
+                      height: 50.0,
+                      width: 200.0,
+                      margin: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(
+                                color: Color.fromRGBO(0, 165, 220, 15))),
+                        padding: EdgeInsets.all(10.0),
+                        color: Color.fromRGBO(0, 165, 220, 1),
+                        textColor: Colors.white,
+                        child: Text('Log In', style: TextStyle(fontSize: 15)),
+                        onPressed: () async {
+                          try {
+                            var user = await Auth.signIn(email, password);
+                          } catch (error) {
+                            setState(() {
+                              loginError = "Incorrect password or e-mail";
+                            });
+                          }
+                          if (user != null) {
+                            Navigator.pushReplacementNamed(context, '/');
+                          }
+                        },
+                      ),
+                    ),
+                    Container(
+                        height: 50.0,
+                        width: 200.0,
+                        margin: EdgeInsets.only(bottom: 10),
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(
+                                  color: Color.fromRGBO(0, 160, 227, 1))),
+                          padding: EdgeInsets.all(10.0),
+                          color: Colors.white,
+                          textColor: Color.fromRGBO(0, 160, 227, 1),
+                          child:
+                              Text('Sign Up', style: TextStyle(fontSize: 15)),
+                          onPressed: () async {
+                            Navigator.pushReplacementNamed(
+                                context, '/register');
+                          },
+                        )),
+                    SizedBox(
+                      height: 70,
+                      width: 100,
+                    ),
+                    ShowError(loginError)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
